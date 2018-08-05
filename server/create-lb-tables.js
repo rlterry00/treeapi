e164c8ee9dd6f6a20dea88402b4895181103adce
rlterry00 <chuckterry00@gmail.com>
2018-08-05 10:28:44 -0400

    rebuild

--- server/create-lb-tables.js
+++
@@ -1,8 +0,0 @@
-var server = require('./server');
-var ds = server.dataSources.db;
-var lbTables = ['branches', 'leaves'];
-ds.automigrate(lbTables, function(er) {
-  if (er) throw er;
-  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
